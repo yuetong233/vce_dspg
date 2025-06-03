82e6c03784dc32b887597f9309997f28efdfc61e
Jeffrey Ogle <you@example.com>
2025-06-03 11:57:14 -0400

    I haven't really done a whole lot here, I just uploaded all of the excel files to the environment under new names.

--- new_folder.R
+++ new_folder.R
@@ -3,4 +3,13 @@
 "This is a test"
 
 #Uploading Excel Files and Editing the Header Names
+library(dplyr)
+library(tidyverse)
 
+install.packages("readxl")
+library(readxl)
+
+install.packages("openxlsx")
+library(openxlsx)
+
+data <- read.xlsx("Participation_2021_2022.xlsx")
