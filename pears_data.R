3cf26eedf0c0a4b0da44cc6ce43a6cdab3b8475e
Jeffrey Ogle <you@example.com>
2025-06-09 13:20:37 -0400

    Tallied up the totals of races and ethnicities, here are the codes.

--- pears_data.R
+++ pears_data.R
@@ -4,12 +4,3 @@ install.packages("readxl")
 
 library(readxl)
 
-pears_action_plan <- read_excel("PEARS Raw Data 5.23.25.xlsx", sheet = "Program Activities")
-
-pears_action_plan_data <- read_excel("PEARS Action-Plans-Export 5.30.25.xlsx", sheet = "Action Plan Data")
-
-pears_outcome_indicators <- read_excel("PEARS Action-Plans-Export 5.30.25.xlsx", sheet = "Outcome Indicators")
-
-pears_raw_data_codebook <- read_excel("PEARS Raw Data 5.23.25.xlsx", sheet = "Program Activities Codebook")
-
-pears_action_plan_codebook <- read_excel("PEARS Action-Plans-Export 5.30.25.xlsx", sheet = "Codebook")
