728ca000f858513664da9491047ed1ea9953537f
Jeffrey Ogle <you@example.com>
2025-06-10 09:24:30 -0400

    Editing the demographics excel sheet

--- pears_data.R
+++ pears_data.R
@@ -133,3 +133,27 @@ new_sheet <- cbind.data.frame(new_column_values, ethnicity_totals_statewide)
 
 new_sheet2 <- rbind(new_sheet, ethnicity_totals_statewide)
 
+
+# June 10 2025 ------------------------------------------------------------
+
+#WILL BE WORKING TO DRAW IN MORE CENSUS VARIABLES, AND OVERLAY
+#OVERLAY THEM WITH 4-H ATTENDANCE, AND ACTION PLAN ATTENDANCE
+#BY ETHNICITY
+
+#CHANGING THE YEARS COLUMN IN THIS EXCEL SHEET
+
+# Define mapping vector
+date_mapping <- c("4/1/2020", "7/1/2020", "7/1/2021", "7/1/2022", "7/1/2023")
+
+
+# Assuming your data frame is called df and the column is 'years'
+va_demographics$YEAR <- date_mapping[va_demographics$YEAR]
+
+install.packages("openxlsx")
+library(openxlsx)
+
+setwd("C:\Users\jeffr\Desktop\diego_and_jeffrey_initial_vce_data")
+
+getwd()
+write.xlsx(va_demographics, "va_demographics_updated.xlsx")
+
