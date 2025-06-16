5de8c9a507bced8cdb6474a99f6212e98701e9dd
Jeffrey Ogle <you@example.com>
2025-06-16 10:07:04 -0400

    Catching up from last week, merged the populations and demographics sheets

--- june12work.R
+++ june12work.R
@@ -1,45 +1,49 @@
 # June 12 2025 ------------------------------------------------------------
 
+library(tidyverse)
+library(dplyr)
+
 #ATTEMPTING TO FURTHER CLEAN AND SUMMARIZE THE DATA
 
 rm(summary_df)
 
+ultra_cleaned_demographic_sheet2 <- counties_and_demographics_june_11
 
 # Summarize numeric columns by sum
-summary_df <- counties_and_demographics_june_11 %>%
+ultra_cleaned_demographic_sheet2 <- ultra_cleaned_demographic_sheet2 %>%
   group_by(`Event Site County`) %>%
   summarize(
-    total_volunteers = sum(`Number of Volunteers`, na.rm = TRUE),
-    total_hours = sum(`Volunteering Hours`, na.rm = TRUE),
-    total_participants = sum(`Total Participants`, na.rm = TRUE),
-    number_sessions = sum(`Number of Sessions`, na.rm = TRUE),
-    male_participants = sum(`Male Participants`, na.rm = TRUE),
-    female_participants = sum(`Female Participants`, na.rm = TRUE),
-    non_binary_participants = sum(`Non-Binary Participants`, na.rm = TRUE),
-    gender_not_respond = sum(`Gender Prefer Not to Respond`, na.rm = TRUE),
-    age_1to5 = sum(`Aged 1-5`, na.rm = TRUE),
-    age_5to7 = sum(`Aged 5-7`, na.rm = TRUE),
-    age_8to10 = sum(`Aged 8-10`, na.rm = TRUE),
-    age_11to13 = sum(`Aged 11-13`, na.rm = TRUE),
-    age_14to17 = sum(`Aged 14-17`, na.rm = TRUE),
-    age_18to29 = sum(`Aged 18-29`, na.rm = TRUE),
-    age_30to59 = sum(`Aged 30-59`, na.rm = TRUE),
-    age_60to75 = sum(`Aged 60-75`, na.rm = TRUE),
-    age_76over = sum(`Aged 76+`, na.rm = TRUE),
-    hispanic_particip = sum(`Total Hispanic Population`, na.rm = TRUE),
-    non_hispanic_particip = sum(`Total Non-Hispanic Population`, na.rm = TRUE),
-    ethnicity_no_response = sum(`Ethnicity Prefer Not to Respond`, na.rm = TRUE),
-    unknown_ethnicity = sum(`Total Participation - Unknown Ethnicity`, na.rm = TRUE),
-    total_natam_one_particip = sum(`Total Native American Participation - One Race`, na.rm = TRUE),
-    total_natam_multi_particip = sum(`Total Native American Participation - Multirace`, na.rm = TRUE),
-    total_asian_one_particip = sum(`Total Asian Participation - One Race`, na.rm = TRUE),
-    total_asain_multi_particip = sum(`Total Asian Participation - Multirace`, na.rm = TRUE),
-    total_afram_one_particip = sum(`Total African American Participation - One Race`, na.rm = TRUE),
-    total_afram_multi_particip = sum(`Total African American Participation - Multirace`, na.rm = TRUE),
-    total_hawpac_one_particip = sum(`Total Hawaiian/Pacific Islander Participation - One Race`, na.rm = TRUE),
-    total_hawpac_multi_particip = sum(`Total Hawaiian/Pacific Islander Participation - Multirace`, na.rm = TRUE),
-    total_white_one_particip = sum(`Total White Participation - One Race`, na.rm = TRUE),
-    total_white_multi_particip = sum(`Total White Participation - Multirace`, na.rm = TRUE),
+    total_volunteers = sum(`Number of Volunteers`, na.rm = FALSE),
+    total_hours = sum(`Volunteering Hours`, na.rm = FALSE),
+    total_participants = sum(`Total Participants`, na.rm = FALSE),
+    number_sessions = sum(`Number of Sessions`, na.rm = FALSE),
+    male_participants = sum(`Male Participants`, na.rm = FALSE),
+    female_participants = sum(`Female Participants`, na.rm = FALSE),
+    non_binary_participants = sum(`Non-Binary Participants`, na.rm = FALSE),
+    gender_not_respond = sum(`Gender Prefer Not to Respond`, na.rm = FALSE),
+    age_1to5 = sum(`Aged 1-5`, na.rm = FALSE),
+    age_5to7 = sum(`Aged 5-7`, na.rm = FALSE),
+    age_8to10 = sum(`Aged 8-10`, na.rm = FALSE),
+    age_11to13 = sum(`Aged 11-13`, na.rm = FALSE),
+    age_14to17 = sum(`Aged 14-17`, na.rm = FALSE),
+    age_18to29 = sum(`Aged 18-29`, na.rm = FALSE),
+    age_30to59 = sum(`Aged 30-59`, na.rm = FALSE),
+    age_60to75 = sum(`Aged 60-75`, na.rm = FALSE),
+    age_76over = sum(`Aged 76+`, na.rm = FALSE),
+    hispanic_particip = sum(`Total Hispanic Population`, na.rm = FALSE),
+    non_hispanic_particip = sum(`Total Non-Hispanic Population`, na.rm = FALSE),
+    ethnicity_no_response = sum(`Ethnicity Prefer Not to Respond`, na.rm = FALSE),
+    unknown_ethnicity = sum(`Total Participation - Unknown Ethnicity`, na.rm = FALSE),
+    total_natam_one_particip = sum(`Total Native American Participation - One Race`, na.rm = FALSE),
+    total_natam_multi_particip = sum(`Total Native American Participation - Multirace`, na.rm = FALSE),
+    total_asian_one_particip = sum(`Total Asian Participation - One Race`, na.rm = FALSE),
+    total_asain_multi_particip = sum(`Total Asian Participation - Multirace`, na.rm = FALSE),
+    total_afram_one_particip = sum(`Total African American Participation - One Race`, na.rm = FALSE),
+    total_afram_multi_particip = sum(`Total African American Participation - Multirace`, na.rm = FALSE),
+    total_hawpac_one_particip = sum(`Total Hawaiian/Pacific Islander Participation - One Race`, na.rm = FALSE),
+    total_hawpac_multi_particip = sum(`Total Hawaiian/Pacific Islander Participation - Multirace`, na.rm = FALSE),
+    total_white_one_particip = sum(`Total White Participation - One Race`, na.rm = FALSE),
+    total_white_multi_particip = sum(`Total White Participation - Multirace`, na.rm = FALSE),
   )
 
 
@@ -51,7 +55,7 @@ write.xlsx(summary_df, "cleaned_demographic_sheet_6.12.25.xlsx")
 
 cleaned_demographic_sheet <- summary_df
 
-cleaned_demographic_sheet <- cleaned_demographic_sheet %>%
+ultra_cleaned_demographic_sheet2 <- ultra_cleaned_demographic_sheet2 %>%
   rename("Total Volunteers" = "total_volunteers",
          "Total Volunteer Hours" = "total_hours",
          "Total Participants" = "total_participants",
@@ -97,7 +101,7 @@ cleaned_demographic_sheet <- cleaned_demographic_sheet %>%
 
 #If you can, create one more column for volunteers who logged hours. 
 #This will let you calculate the average number of hours volunteered 
-#per volunteer. The new column is necessary because, e.g., none of the first 
+#per volunteer. The new column is necessary because, e.g., one of the first 
 #lines has one volunteer and no hours. Adding this column lets you determine 
 #average hours volunteered in an unbiased way.
 
@@ -109,3 +113,132 @@ cleaned_demographic_sheet <- cleaned_demographic_sheet %>%
 
 
 # -------------------------------------------------------------------------
+
+
+# June 13 2025 ------------------------------------------------------------
+
+
+ultra_cleaned_demographic_sheet2 <- counties_and_demographics_june_11
+rm(counties_and_demographics_june_11)
+
+
+# Create a new column for volunteers who reported hours
+ultra_cleaned_demographic_sheet2$Reported_Hours <- ifelse(!is.na(ultra_cleaned_demographic_sheet2$`Volunteering Hours`), 
+                                                                     ultra_cleaned_demographic_sheet2$`Total Volunteer Hours`, NA)
+
+# Create a new column for volunteers who did not report hours
+ultra_cleaned_demographic_sheet2$No_Reported_Hours <- ifelse(is.na(ultra_cleaned_demographic_sheet2$`Volunteering Hours`), "Did Not Report", "Reported")
+
+# Alternatively, if you just want TRUE/FALSE
+# df$Reported <- !is.na(df$VolunteeringHours)
+rm(ultra_cleaned_demographic_sheet)
+
+names(ultra_cleaned_demographic_sheet2)
+
+
+# Reorder to put 'NoReportedHours' first
+ultra_cleaned_demographic_sheet2 <- ultra_cleaned_demographic_sheet2[c("Volunteers_Reported_Hours", setdiff(names(ultra_cleaned_demographic_sheet2), "Volunteers_Reported_Hours"))]
+
+
+# Verify change
+names(df)
+
+ultra_cleaned_demographic_sheet2 <- ultra_cleaned_demographic_sheet2[ , -1]
+ultra_cleaned_demographic_sheet2 <- ultra_cleaned_demographic_sheet2[ , -47]
+
+# Create a new column 'LoggedHours' that is TRUE if hours are reported, FALSE if not
+ultra_cleaned_demographic_sheet2$LoggedHours <- !is.na(ultra_cleaned_demographic_sheet2$`Volunteering Hours`)
+
+# Calculate average hours for volunteers who reported hours
+avg_hours <- mean(ultra_cleaned_demographic_sheet2$`Volunteering Hours`, na.rm = TRUE)
+
+
+# Specify the column name
+column_name <- "LoggedHours"
+
+# Get all other columns
+other_columns <- setdiff(names(ultra_cleaned_demographic_sheet2), column_name)
+
+# Create new order: first 8 columns, then the target column, then the rest
+new_order <- append(other_columns, column_name, after = 8)
+
+# Reorder the data frame
+ultra_cleaned_demographic_sheet2 <- ultra_cleaned_demographic_sheet2[new_order]
+
+
+ultra_cleaned_demographic_sheet2 <- ultra_cleaned_demographic_sheet2 %>%
+  rename("Logged Hours" = "LoggedHours")
+
+checking_columns <- sapply(ultra_cleaned_demographic_sheet2, is.numeric)
+print(checking_columns)
+
+# Apply to all numeric columns
+ultra_cleaned_demographic_sheet2[checking_columns] <- lapply(ultra_cleaned_demographic_sheet2[checking_columns], function(col) {
+  col <- as.character(col)          # Convert to character
+  col[is.na(col)] <- "No Data"       # Replace NA with "No Data"
+  return(col)
+})
+
+rm(summary_df, cleaned_demographic_sheet, counties_and_demographics_june_11,)
+
+
+
+# June 16th, 2025 ---------------------------------------------------------
+
+#TRYING AGAIN TO CREATE THE SECTIONS FOR LOGGED AND UNLOGGED HOURS
+
+library(dplyr)
+
+ultra_cleaned_demographic_sheet2 <- ultra_cleaned_demographic_sheet2 %>%
+  mutate(
+    Hours_Logged = if_else(!is.na(`Total Volunteer Hours`), `Total Volunteer Hours`, NA_real_),
+    No_Hours_Logged = if_else(is.na(`Total Volunteer Hours`), "No", NA_character_)
+  )
+
+
+# Get the current column names
+col_names <- colnames(ultra_cleaned_demographic_sheet2)
+
+# Reorder columns: put the new columns in positions 4 and 5
+new_order <- c(
+  col_names[1:3],             # columns 1 to 3
+  "Hours_Logged",             # 4th position
+  "No_Hours_Logged",          # 5th position
+  col_names[(4:length(col_names))]  # remaining columns
+)
+
+# Reorder the data frame
+ultra_cleaned_demographic_sheet2 <- ultra_cleaned_demographic_sheet2 %>%
+  select(all_of(new_order))
+
+ultra_cleaned_demographic_sheet2 <- ultra_cleaned_demographic_sheet2 %>%
+  rename("Volunteer Hours Logged" = "Hours_Logged",
+         "Volunteer No Hours Logged" = "No_Hours_Logged")
+
+# Calculate average hours for volunteers who reported hours
+avg_hours <- mean(ultra_cleaned_demographic_sheet2$`Volunteering Hours`, na.rm = TRUE)
+
+library(dplyr)
+
+# Replace NA with blank ("")
+ultra_cleaned_demographic_sheet2[is.na(ultra_cleaned_demographic_sheet2)] <- "No Data"
+
+# Or replace NA with zero
+library(readxl)
+library(writexl)
+
+
+# Loop through each column and replace NA with "No Data" only if the column is character
+ultra_cleaned_demographic_sheet2 <- ultra_cleaned_demographic_sheet2 %>%
+  mutate(across(where(is.character), ~replace(., is.na(.), "No Data")))
+
+
+# Save the modified data
+library(writexl)
+write_xlsx(ultra_cleaned_demographic_sheet2, "june16_cleaned_data.xlsx")
+
+setwd("C:\\Users\\jeffr\\Desktop\\VCE Excel Data\\Clean Data")
+
+merged_data_june16_2025 <- bind_rows(june16_cleaned_data, county_populations_virginia_2020_2024)
+
+
